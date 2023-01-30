83cf8029cb02df74bec3c7eb75b15c1d36653cb0
Sergey Ugryumov <physiho@yahoo.com>
2023-01-30 16:32:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-githubToken = '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'
+telegramToken = "TELETOKEN"
 
-geniusToken = 'MUtakF0ShKREXrR5MSC1_vrsCpWwcx0MBh5k9GINn2WT2tYqqsQ9kfJy5C8Lr2Gv'
-
-# spotify auth:
-client_id = "d57ce86eacaf4f4282d1da003edc2275"
-client_secret = "1e8652aa32dc4c4a94bab6613bf8f085"
-redirect_uri = "http://127.0.0.1:9090"
-url = 'https://accounts.spotify.com/authorize?client_id=d57ce86eacaf4f4282d1da003edc2275&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A9090&scope=user-read-currently-playing'
-
-telegramToken = "5453334258:AAGTg24fHyY_WpuB6Cc8uo2hWBtjJN-nhzM"
-
-openaiToken = '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'
+openaiToken = 'AITOKEN'
